908686f00b51f2b090542bf6345b25aa5929d600
Joselu <joseluissgz97@gmail.com>
2024-10-10 16:39:37 +0200

    add:  CartProduct interface

--- src/interfaces/product.interface.ts
+++ src/interfaces/product.interface.ts
@@ -11,6 +11,16 @@ export interface Product {
   // type: Type;
   gender: Category;
 }
+
+export interface CartProduct {
+  id: string;
+  slug: string;
+  title: string;
+  price: number;
+  quantity: number;
+  size: Size;
+  image: string;
+}
 export type Category = 'men' | 'women' | 'kid' | 'unisex';
 export type Size = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
 export type Type = 'shirts' | 'pants' | 'hoodies' | 'hats';
