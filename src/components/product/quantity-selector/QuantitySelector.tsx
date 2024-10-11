1497beab6199b8f2cb73e88e0d22e31ac63aa5de
Joselu <joseluissgz97@gmail.com>
2024-10-11 15:01:00 +0200

    refactor: Use custom hook for quantity selector and simplify quantity increment and decrement functions

--- src/components/product/quantity-selector/QuantitySelector.tsx
+++ src/components/product/quantity-selector/QuantitySelector.tsx
@@ -3,26 +3,18 @@ import { IoAddOutline, IoRemoveOutline } from 'react-icons/io5';
 
 type Props = {
   quantity: number;
-  onIncrementeQuatity: (quantity: number) => void;
-  onDecrementeQuatity: (quantity: number) => void;
+  onIncrementeQuatity: () => void;
+  onDecrementeQuatity: () => void;
 };
 
 export const QuantitySelector = ({ quantity, onIncrementeQuatity, onDecrementeQuatity }: Props) => {
-  const handleDecrementeQuatity = (value: number) => {
-    if (quantity <= 1) return;
-    onDecrementeQuatity(quantity - value);
-  };
-  const handleIncrementeQuatity = (value: number) => {
-    if (quantity >= 10) return;
-    onIncrementeQuatity(quantity + value);
-  };
   return (
     <div className="bg-neutral-gray py-2 px-8 flex items-center justify-between gap-2 rounded-full basis-40">
-      <button onClick={() => handleDecrementeQuatity(1)}>
+      <button onClick={onDecrementeQuatity}>
         <IoRemoveOutline />
       </button>
       <span>{quantity}</span>
-      <button onClick={() => handleIncrementeQuatity(1)}>
+      <button onClick={onIncrementeQuatity}>
         <IoAddOutline />
       </button>
     </div>
