8cf283210b124295382ccd9ea2ed29a8c7574a64
Joselu <joseluissgz97@gmail.com>
2024-10-14 22:14:53 +0200

    add: loging for with login action

--- src/actions/auth/login.ts
+++ src/actions/auth/login.ts
@@ -5,15 +5,18 @@ import { AuthError } from 'next-auth';
 
 export const authenticate = async (prevState: string | undefined, formData: FormData) => {
   try {
-    console.log(Object.fromEntries(formData));
-    await signIn('credentials', formData);
+    await signIn('credentials', {
+      ...Object.fromEntries(formData),
+      redirect: false,
+    });
+    return 'Success';
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
         case 'CredentialsSignin':
-          return 'Invalid credentials.';
+          return 'Invalid credentials';
         default:
-          return 'Something went wrong.';
+          return 'Something went wrong';
       }
     }
     throw error;
