a77277380304cc6bec8cf5c28a94ebe3fca34f1b
Joselu <joseluissgz97@gmail.com>
2024-10-14 22:12:44 +0200

    add: add Spinner

--- src/components/ui/spinner/Spinner.tsx
+++ src/components/ui/spinner/Spinner.tsx
@@ -1,7 +1,11 @@
 import './spinder.css';
-export const Spinner = (props: React.SVGProps<SVGSVGElement>) => {
+type Props = {
+  white?: boolean;
+};
+export const Spinner = ({ white }: Props) => {
+  const colorStyle = white ? 'white' : 'black';
   return (
-    <svg {...props} className="container" viewBox="0 0 40 40">
+    <svg className={`container ${colorStyle}`} viewBox="0 0 40 40">
       <circle className="track" cx="20" cy="20" r="17.5" pathLength="100" strokeWidth="5px" fill="none" />
       <circle className="car" cx="20" cy="20" r="17.5" pathLength="100" strokeWidth="5px" fill="none" />
     </svg>
