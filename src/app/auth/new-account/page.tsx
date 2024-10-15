1959584cb201ed4009aac06754f1763167b4d44c
Joselu <joseluissgz97@gmail.com>
2024-10-15 18:58:00 +0200

    add: input for name and creating the new user with autoLogin

--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,27 +1,35 @@
 'use client';
-import { InputForm, Logo } from '@/components';
+import { login, registerUser } from '@/actions';
+import { InputForm, Logo, Spinner } from '@/components';
 import { titleFont } from '@/config/fonts';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Link from 'next/link';
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { registerSchema, RegisterSchema } from './registerSchema';
 
 const NewAccountPage = () => {
+  const [registerError, setRegisterError] = useState<string | null>(null);
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<RegisterSchema>({
     resolver: zodResolver(registerSchema),
   });
 
-  const onhandleSubmit: SubmitHandler<RegisterSchema> = (data) => {
-    const obejestToSend = {
-      ...data,
-      test: 'Test',
-    };
-    console.log(obejestToSend);
+  const onhandleSubmit: SubmitHandler<RegisterSchema> = async (data) => {
+    const resp = await registerUser(data.name, data.email, data.password);
+
+    if (!resp.ok) {
+      return setRegisterError(resp.message);
+    }
+
+    await login(data.email.toLocaleLowerCase(), data.password);
+
+    window.location.replace('/');
   };
+
   return (
     <section className="wrapper h-dvh p-4 lg:grid lg:grid-cols-2">
       <div className="hidden p-4 h-full lg:flex flex-col justify-center gap-4">
@@ -37,6 +45,14 @@ const NewAccountPage = () => {
       <div className="h-full flex flex-col gap-4 justify-center">
         <h1 className={`${titleFont.className} text-xl font-black`}>Creante Account</h1>
         <form className="flex flex-col gap-4" onSubmit={handleSubmit(onhandleSubmit)}>
+          <InputForm
+            type="text"
+            label="Name"
+            placeholder="your name"
+            error={errors['name']}
+            id="name"
+            {...register('name')}
+          />
           <InputForm
             type="email"
             label="Email"
@@ -54,8 +70,15 @@ const NewAccountPage = () => {
             {...register('password')}
           />
 
-          <button type="submit" className="w-full px-4 py-2 rounded-md bg-pure-black text-neutral-gray">
-            Register
+          {registerError && (
+            <span className="px-2 py-1 rounded-sm bg-red-700 text-neutral-gray">{registerError}</span>
+          )}
+
+          <button
+            type="submit"
+            className="w-full px-4 py-2 rounded-md bg-pure-black text-neutral-gray flex items-center justify-center gap-2"
+          >
+            Register {isSubmitting && <Spinner white />}
           </button>
         </form>
         <span className="text-sumary-color font-medium">
--- src/app/auth/new-account/registerSchema.ts
+++ src/app/auth/new-account/registerSchema.ts
@@ -1,6 +1,11 @@
 import { z } from 'zod';
 
 export const registerSchema = z.object({
+  name: z
+    .string({
+      required_error: 'Name must be a string',
+    })
+    .min(1, { message: 'Name is required' }),
   email: z
     .string({
       required_error: 'Email is required',
