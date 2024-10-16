4e46055e7fb08101411d7e6fb4918593358de684
Joselu <joseluissgz97@gmail.com>
2024-10-16 23:49:14 +0200

    add: getCountries and getAddress server action

---
+++ src/actions/country/getCountries.ts
@@ -0,0 +1,16 @@
+'use server';
+
+import prisma from '@/lib/prisma';
+
+export const getCountries = async () => {
+  try {
+    const countries = await prisma.country.findMany({
+      orderBy: {
+        name: 'asc',
+      },
+    });
+    return countries;
+  } catch (error) {
+    return [];
+  }
+};
--- src/app/(shop)/checkout/address/addresSchema.ts
+++ src/app/(shop)/checkout/address/addresSchema.ts
@@ -10,12 +10,13 @@ export const AddressSchema = z.object({
   address: z.string({ required_error: 'Address is required' }).min(1, { message: 'Address cant be empty' }),
   address2: z.string().optional(),
   zipCode: z
-    .number({ required_error: 'ZipCode is required' })
+    .string({ required_error: 'ZipCode is required' })
     .min(1, { message: 'ZipCode cant be empty' })
-    .refine((code) => !isNaN(Number(code)), { message: 'City must be a number' }),
+    .refine((code) => !isNaN(Number(code)), { message: 'Zip code must be a number' }),
   city: z.string({ required_error: 'City is required' }).min(1, { message: 'City cant be empty' }),
   country: z.string({ required_error: 'Country is required' }).min(1, { message: 'Country cant be empty' }),
   phone: z.string({ required_error: 'Phone is required' }).min(1, { message: 'Phone cant be empty' }),
+  rememberAddress: z.boolean().optional().default(false),
 });
 
 export type Address = z.infer<typeof AddressSchema>;
